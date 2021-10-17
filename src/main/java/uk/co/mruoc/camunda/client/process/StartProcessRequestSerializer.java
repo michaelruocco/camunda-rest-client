a325429870d586d099a0b895bc21b22cb23c902a
Michael Ruocco <michael.ruocco@hotmail.com>
2021-10-17 09:47:31 +0100

    refactors to remove uncovered lines that wernt actually needed

--- src/main/java/uk/co/mruoc/camunda/client/process/StartProcessRequestSerializer.java
+++ src/main/java/uk/co/mruoc/camunda/client/process/StartProcessRequestSerializer.java
@@ -3,7 +3,6 @@ package uk.co.mruoc.camunda.client.process;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import org.apache.commons.lang3.StringUtils;
 import uk.co.mruoc.camunda.client.process.variable.Variable;
 
 import java.io.IOException;
@@ -21,23 +20,9 @@ public class StartProcessRequestSerializer extends StdSerializer<StartProcessReq
     @Override
     public void serialize(StartProcessRequest request, JsonGenerator json, SerializerProvider provider) throws IOException {
         json.writeStartObject();
-        writeVariables(json, request.getVariables());
-        writeBusinessKey(json, request.getBusinessKey());
+        provider.defaultSerializeField("variables", toMap(request.getVariables()), json);
+        provider.defaultSerializeField("businessKey", request.getBusinessKey(), json);
         json.writeEndObject();
-    }
-
-    private static void writeVariables(JsonGenerator json, Collection<Variable> variables) throws IOException {
-        if (variables.isEmpty()) {
-            return;
-        }
-        json.writeObjectField("variables", toMap(variables));
-    }
-
-    private static void writeBusinessKey(JsonGenerator json, String businessKey) throws IOException {
-        if (StringUtils.isEmpty(businessKey)) {
-            return;
-        }
-        json.writeStringField("businessKey", businessKey);
     }
 
     private static Map<String, Variable> toMap(Collection<Variable> variables) {
