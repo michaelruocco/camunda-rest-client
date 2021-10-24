4887d93ca5c570bc72f2e5ec9b0f8a09860c1884
Michael Ruocco <michael.ruocco@hotmail.com>
2021-10-24 12:00:14 +0100

    adds test coverage

--- src/test/java/uk/co/mruoc/camunda/client/ResponseConverterTest.java
+++ src/test/java/uk/co/mruoc/camunda/client/ResponseConverterTest.java
@@ -1,11 +1,13 @@
 package uk.co.mruoc.camunda.client;
 
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.jupiter.api.Test;
 import uk.co.mruoc.json.JsonConverter;
 
 import java.net.http.HttpResponse;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -41,6 +43,24 @@ class ResponseConverterTest {
     }
 
     @Test
+    void shouldDoNothingIfStatusCodeIs200() {
+        HttpResponse<String> httpResponse = givenResponse(200);
+
+        ThrowingCallable call = () -> responseConverter.throwErrorIfRequired(httpResponse);
+
+        assertThatCode(call).doesNotThrowAnyException();
+    }
+
+    @Test
+    void shouldDoNothingIfStatusCodeIs299() {
+        HttpResponse<String> httpResponse = givenResponse(299);
+
+        ThrowingCallable call = () -> responseConverter.throwErrorIfRequired(httpResponse);
+
+        assertThatCode(call).doesNotThrowAnyException();
+    }
+
+    @Test
     void shouldThrowExceptionIfStatusCodeIsBelow2XXRange() {
         HttpResponse<String> response = givenResponse(199);
 
@@ -56,6 +76,28 @@ class ResponseConverterTest {
         HttpResponse<String> response = givenResponse(300);
 
         Throwable error = catchThrowable(() -> responseConverter.toTypeOrThrowError(response, Object.class));
+
+        assertThat(error)
+                .isInstanceOf(CamundaClientException.class)
+                .hasMessage(BODY);
+    }
+
+    @Test
+    void shouldThrowExceptionIfStatusCodeIsBelow2XXRangeWithNoType() {
+        HttpResponse<String> response = givenResponse(199);
+
+        Throwable error = catchThrowable(() -> responseConverter.throwErrorIfRequired(response));
+
+        assertThat(error)
+                .isInstanceOf(CamundaClientException.class)
+                .hasMessage(BODY);
+    }
+
+    @Test
+    void shouldThrowExceptionIfStatusCodeIsAbove2XXRangeWithNoType() {
+        HttpResponse<String> response = givenResponse(300);
+
+        Throwable error = catchThrowable(() -> responseConverter.throwErrorIfRequired(response));
 
         assertThat(error)
                 .isInstanceOf(CamundaClientException.class)
